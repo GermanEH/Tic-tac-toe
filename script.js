57752cf811c6ce261ee0205477867c0c0ca73f57
German Horianski <germanezequielhorianski@gmail.com>
2024-04-08 12:25:22 -0300

    Feat. Create board logic

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ function Cell() {
 }
 
 function Board() {
-  const boardCells = [];
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board.push(Cell());
+    }
+  }
 
   const getBoardCells = () => boardCells;
 
