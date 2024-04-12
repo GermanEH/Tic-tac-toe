3071dd9e71a47cf61a4e9b4bc561c1d328666baa
German Horianski <germanezequielhorianski@gmail.com>
2024-04-12 18:13:07 -0300

    Fix. Change markCell function to adapt it to new board type structure - not 2d array anymore, now 1d array

--- js/game/Board.js
+++ js/game/Board.js
@@ -4,16 +4,13 @@ export default function Board() {
   const board = [];
 
   for (let i = 0; i < 9; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board.push(Cell());
-    }
+    board.push(Cell());
   }
 
   const getBoardCells = () => board;
 
-  const markCell = (cell, mark) => {
-    const cell = board[cell.row][cell.column];
+  const markCell = (selectedCell, mark) => {
+    const cell = board[selectedCell];
     const value = cell.getValue();
 
     if (value !== 0) {
