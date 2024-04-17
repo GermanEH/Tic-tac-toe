ab49871b90d4b31889ce604d364497cbe63c126c
German Horianski <germanezequielhorianski@gmail.com>
2024-04-16 23:47:26 -0300

    Feat. Create loginAudio

--- js/ui/GameScreen.js
+++ js/ui/GameScreen.js
@@ -11,4 +11,26 @@ export default function GameScreen(players, theme) {
   const board = GameBoard();
 
   const game = GameController(players, theme, board);
+
+  renderPlayers(players, theme);
+
+  let $audio = document.getElementById('loginAudio');
+  // let $playerBoard = document.createElement('img');
+  // $playerBoard.src = './medieval_board_cut.jpg';
+  // $gameContainer.appendChild($playerBoard);
+  $audio.src =
+    theme === 'Medieval'
+      ? 'medieval_background.mp3'
+      : theme === 'Futuristic'
+      ? 'futuristic_audio.mp3'
+      : 'victorian_audio.mp3';
+  $audio.play();
+
+  let $attackSound = document.createElement('audio');
+  $attackSound.src =
+    theme === 'Medieval'
+      ? 'sword_attack.mp3'
+      : theme === 'Futuristic'
+      ? 'spacecraft_attack.mp3'
+      : 'gun_attack.mp3';
 }
