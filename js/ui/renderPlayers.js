ea55ebade267023d61450ac83fcf47c70d143555
German Horianski <germanezequielhorianski@gmail.com>
2024-04-18 10:27:09 -0300

    Chore. Refactor Player Two render code by cloning Player One

--- js/ui/renderPlayers.js
+++ js/ui/renderPlayers.js
@@ -64,44 +64,14 @@ export default function renderPlayers(players, theme) {
 
   //PlayerTwo
 
-  let $playerTwo = document.querySelector('.playerTwo');
-  let $playerTwoPicture = document.createElement('div');
-  $playerTwoPicture.style.background =
-    theme === 'Medieval'
-      ? 'url(./ui/frames/medieval_frame.png)'
-      : theme === 'Futuristic'
-      ? 'url(./ui/frames/futuristic_frame.png)'
-      : 'url(./ui/frames/vintage_frame.png)';
-  $playerTwoPicture.style.backgroundSize = 'contain';
-  $playerTwoPicture.style.backgroundRepeat = 'no-repeat';
-  $playerTwoPicture.classList.add('playerPicture');
-  //  theme === 'Medieval';
-  // ? 'url(medieval_board.png)'
-  // : theme === 'Futuristic'
-  // ? 'url(futuristic_panel.jpg)'
-  // :
-  let $playerTwoBoard = document.createElement('div');
-  $playerTwoBoard.style.background =
-    theme === 'Medieval'
-      ? 'url(./medieval_board.png)'
-      : theme === 'Futuristic'
-      ? 'url(./futuristic_panel.jpg)'
-      : 'url(./vintage_board.png)';
-  $playerTwoBoard.style.backgroundRepeat = 'no-repeat';
-  $playerTwoBoard.style.backgroundSize = 'contain';
-  $playerTwoBoard.classList.add('playerBoard');
-  const playerTwo = document.createElement('div');
+  let $playerTwo = $playerOne.cloneNode(true);
+  $playerTwo.classList.add('.playerTwo');
+  let $main = document.querySelector('.gameContainer main');
+  $main.appendChild($playerTwo);
+  let $playerTwoBoard = $playerTwo.querySelector('.playerBoard');
+  const playerTwo = $playerTwo.querySelector('.playerBoard div');
   playerTwo.textContent = players[1]?.name || 'Player Two';
-  $playerTwoBoard.appendChild(playerTwo);
-
-  let $playerTwoBoardSelect = document.createElement('select');
-  weapons.forEach((weapon) => {
-    const $weapon = document.createElement('option');
-    $weapon.value = weapon.value;
-    $weapon.text = weapon.value;
-    $playerTwoBoardSelect.appendChild($weapon);
-  });
-
+  let $playerTwoBoardSelect = $playerTwo.querySelectorAll('select')[1];
   $playerTwoBoardSelect.addEventListener('change', function () {
     game.changeWeapon(
       players[1]?.name || 'Player Two',
@@ -109,17 +79,7 @@ export default function renderPlayers(players, theme) {
     );
   });
 
-  const $playerTwoBoardColor = document.createElement('select');
-
-  colors.forEach((color) => {
-    const $color = document.createElement('option');
-    $color.value = color;
-    $color.text = color;
-    $playerTwoBoardColor.appendChild($color);
-  });
-
-  $playerTwoBoard.appendChild($playerTwoBoardColor);
-  $playerTwoBoard.appendChild($playerTwoBoardSelect);
+  const $playerTwoBoardColor = $playerTwo.querySelector('select');
 
   $playerTwoBoardColor.addEventListener('change', function () {
     game.changeColor(
@@ -128,9 +88,6 @@ export default function renderPlayers(players, theme) {
     );
   });
 
-  $playerTwo.appendChild($playerTwoPicture);
-  $playerTwo.appendChild($playerTwoBoard);
-  // $playerTwo.style.transform = 'scaleY(1.5)';
   if (players[0].profileImage) {
     let $profileImageOne = document.createElement('img');
     $profileImageOne.src = players[0].profileImage?.name;
@@ -139,11 +96,9 @@ export default function renderPlayers(players, theme) {
     $playerOne.appendChild($profileImageOne);
   }
   if (players[1]?.profileImage) {
-    let $profileImageTwo = document.createElement('img');
+    let $profileImageTwo =
+      $playerTwo.querySelector('img') || $playerTwo.createElement('img');
     $profileImageTwo.src = players[1].profileImage.name;
-    $profileImageTwo.style.width = '8vw';
-    $profileImageTwo.style.height = '8vw';
-    $playerTwo.appendChild($profileImageTwo);
   }
 
   return { $playerOne, $playerTwo, $playerOneBoard, $playerTwoBoard };
